f270db8248e7b7ea1aa6ac182a80659062e82674
Txl3r <Harvey.Tyler@live.jobcorps.org>
2022-05-06 19:27:40 +0000

    made changes

--- src/api/routes.py
+++ src/api/routes.py
@@ -16,3 +16,17 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+@api.route('/signup', methods=['GET'])
+def get_user():
+    user = User.query.all()
+    user_list = list(map(lambda x: x.serialize(), user))
+    return jsonify(user_list), 200
+
+@api.route('/signup', methods=['POST'])
+def create_user():
+    request_body = request.get_json()
+    new_user = User(email=request_body["email"], password=request_body["password"], is_active=request_body["is_active"])
+    db.session.add(new_user)
+    db.session.commit()
+    return f"The new user {request_body['email']} was created successfully", 200
